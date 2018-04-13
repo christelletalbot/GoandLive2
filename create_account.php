861f2d69b3dd136efa3fa16e05cf7d9d425667a7
Celine2 <celine.calmels@gmail.com>
2018-04-13 10:05:50 +0200

    ajout create account et welcome page 2

--- create_account.php
+++ create_account.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!--!DOCTYPE html>
 <html lang="fr">
 <head>
 	<meta charset="utf-8" />
@@ -64,6 +64,86 @@
   	<script src="js/bootstrap/bootstrap.bundle.js"></script>
   	<script src="js/app.js"></script>
 
+
+</body>
+</html-->
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="css/style.css"/>
+	<title>Create_Account</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+
+<body>
+	<header></header>
+
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<ul>
+					<li>
+						<button type="button" class="btn-default btn-lg">
+							<a href="#">lorem</a>
+						</button>
+					</li>
+					<li>
+						<button type="button" class="btn-default btn-lg">
+							<a href="#">lorem</a>
+						</button>
+					</li>
+					<li>
+						<button type="button" class="btn-default btn-lg">
+							<a href="#">lorem</a>
+						</button>	
+					</li>
+					<li>
+						<button type="button" class="btn-default btn-lg">
+							<a href="#">lorem</a>	
+						</button>	
+					</li>
+				</ul>
+			</div>
+		</div>
+		
+	</nav>
+	<br>
+
+	<div class="container">
+		<h1>Création de compte</h2>
+		
+		<button type="submit" class="btn btn-primary">via Facebook</button>
+		<br><br>
+		<button type="submit" class="btn btn-warning">via mon adresse mail</button>
+
+		<form action="/action_page.php">
+			<div class="form-group">
+				<label for="surname">Nom:</label>
+	    		<input type="surname" class="form-control" id="email">
+			</div>
+			<div class="form-group">
+				<label for="firstName">prénom:</label>
+	    		<input type="firstName" class="form-control" id="email">
+			</div>
+			<div class="form-group">
+				<label for="email">Adresse mail:</label>
+	    		<input type="email" class="form-control" id="email">
+			</div>
+			<div class="form-group">
+	    		<label for="pwd">Mot de passe:</label>
+	    		<input type="password" class="form-control" id="pwd">
+	  		</div>
+	  		<button type="submit" class="btn btn-default">Créer compte</button>
+		</form>
+	</div>
+	
+	<footer></footer>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </body>
 </html>
--- welcome_page2.php
+++ welcome_page2.php
@@ -1,6 +1,6 @@
 <?php include 'header.php'; ?>
 
-	<nav class="navbar navbar-default">
+	<!--nav class="navbar navbar-default">
 		<div class="container-fluid">
 			<div class="navbar-header">		
 				<ul>
@@ -29,6 +29,7 @@
 		</div>	
 	</nav>
 
+
 	<div class="form">
 		<h1>Cagnottes publiques</h1>
 		<form>
@@ -45,6 +46,121 @@
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			</p>
 		</a>
+	</div-->
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="css/style.css"/>
+	<title>welcome page 2</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  
+</head>
+<body>
+	<header>
+	<div class="page-header">
+		<img src="#">
+		
 	</div>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">	
+				<ul>
+					<li>
+						<button type="button" class="btn-default btn-lg">
+							<a href="#">Cagnottes privées</a>
+						</button>
+					</li>
+					<li>
+						<button type="button" class="btn-default btn-lg">
+							<a href="#">Effectuer un don </a>	
+						</button>	
+					</li>
+					<li>
+						<button type="button" class="btn-default btn-lg">
+							<a href="#">Créer une cagnotte</a>	
+						</button>	
+					</li>
+					<li>
+						<button type="button" class="btn-default btn-lg">
+							<a href="#">Nous contacter</a>
+						</button>	
+					</li>
+				</ul>
+			</div>
+		</div>	
+	</nav>
+	</header>
+
+	
+
+	<div class="container">
+	<h1>Cagnottes publiques</h1>
+	<br><br>
+	
+
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	  <!-- Indicators -->
+	  <ol class="carousel-indicators">
+	    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+	    <li data-target="#myCarousel" data-slide-to="1"></li>
+	    <li data-target="#myCarousel" data-slide-to="2"></li>
+	  </ol>
+
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner">
+	    <div class="item active">
+	      <img src="https://cdn.pixabay.com/photo/2018/04/10/02/07/panoramic-3306144_960_720.jpg" alt="cagnotte1">
+	      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	      </p>
+	    </div>
+
+	    <div class="item">
+	      <img src="https://cdn.pixabay.com/photo/2018/04/10/02/07/panoramic-3306144_960_720.jpg" alt="cagnotte2">
+	      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	    </div>
+
+	    <div class="item">
+	      <img src="https://cdn.pixabay.com/photo/2018/04/10/02/07/panoramic-3306144_960_720.jpg" alt="cagnotte3">
+	      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	    </div>
+	  </div>
+
+	  <!-- Left and right controls -->
+	  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+	    <span class="glyphicon glyphicon-chevron-left"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+	    <span class="glyphicon glyphicon-chevron-right"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+
+	</div>
+	
+	<footer><img src="#"></footer>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
 <?php include 'footer.php'; ?>
 
